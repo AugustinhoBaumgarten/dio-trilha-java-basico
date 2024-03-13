4cd7a21e0bd5f1b7adb419e452878ac044bf9da8
Augustinho <augustinhoo01@hotmail.com>
2024-03-13 20:00:11 -0300

    finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para nosso usuário
+
+        //Obter pela Scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+        Scanner scanner = new Scanner(System.in);
+
+        int Numero;
+        String Agencia, NomeCliente;
+        double Saldo;
+
+        System.out.println("Por favor, digite o número da Agência");
+        
+        Numero = scanner.nextInt();
+
+        System.out.println("");
     }
 }
